d4ff92caa117bc07a7ec8e59c28c3f6e57fe2745
Hai Luong Quang <hluong@gotitapp.co>
2018-09-11 21:29:25 +0700

    Init contract and unit test

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,38 @@ import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
 contract Token is ERC20 {
 
+    // EVENTS
+    event ChainSend(
+        bytes32 host,
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+
+    event ChainReceive(
+        address indexed to,
+        uint256 value
+    );
+
     constructor(uint _amount) public {
         _mint(msg.sender, _amount);
+    }
+
+    function chainSend(bytes32 host, address to, uint256 value) public returns(bool) {
+        _burn(msg.sender, value);
+
+        emit ChainSend(host, msg.sender, to, value);
+
+        return true;
+    }
+
+    function chainReceive(address to, uint256 value) public returns(bool) {
+        // Mint more token
+        _mint(to, value);
+
+        emit ChainReceive(to, value);
+
+        return true;
     }
 }
 
---
+++ test/token.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const BN = require('bignumber.js');
+
+const Token = artifacts.require('Token.sol');
+
+const config = JSON.parse(fs.readFileSync('./conf/config.json'));
+const tokenConfig = config.token;
+
+const bigTen = number => new BN(number.toString(10), 10);
+
+contract('Token', (accounts) => {
+    describe('Function: Transfer token between chains', () => {
+        it('Send token', async () => {
+            let acc = accounts[0];
+            const token = await Token.deployed();
+
+            const amount = 1000;
+            const totalSupply = bigTen(tokenConfig.supply);
+
+            await token.chainSend("server", acc, amount);
+
+            assert.equal(
+                (await token.totalSupply.call()), totalSupply - amount, 'Tokens were burned',
+            )
+
+            assert.equal(
+                (await token.balanceOf.call(acc)), totalSupply - amount, 'Tokens were transferred',
+            )
+
+        })
+
+        it('Receive token', async () => {
+            let acc = accounts[0];
+            const token = await Token.deployed();
+
+            const amount = 1000;
+            const totalSupply = bigTen(tokenConfig.supply);
+
+            await token.chainReceive(acc, amount);
+
+            assert.equal(
+                (await token.totalSupply.call()).toNumber(), totalSupply, 'Tokens were mint'
+            )
+
+            assert.equal(
+                (await token.balanceOf.call(acc)).toNumber(), totalSupply, 'Tokens were transferred'
+            )
+        })
+    })
+})
